35f3f27d596df0ab3b1c7b7e95e9d9118a6aaeb9
Henry Baum <henryandrewbaum@gmail.com>
2020-11-22 02:03:48 -0500

    Products now have a quantity property, seed path edited to reflect this

--- models/products-seed.js
+++ models/products-seed.js
@@ -8,18 +8,22 @@ module.exports = [
       name: "Product 001",
       img: "https://www.mvmt.com/dw/image/v2/BDKZ_PRD/on/demandware.static/-/Sites-mgi-master/default/dw59410f08/images/products/BT01-BLUS_fr.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      price: 250
+      price: 250,
+      qty: 5
+
     },
     {
       name: "Product 002",
       img: "https://www.mvmt.com/dw/image/v2/BDKZ_PRD/on/demandware.static/-/Sites-mgi-master/default/dwe4487569/images/products/28000096_fr.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ",
-      price: 200
+      price: 200,
+      qty: 1
     },
     {
       name: "Product 003",
       img: "https://www.mvmt.com/dw/image/v2/BDKZ_PRD/on/demandware.static/-/Sites-mgi-master/default/dwd0b0e6d3/images/products/28000108_fr.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      price: "180"
+      price: 180,
+      qty: 0
     }
 ];
--- models/products.js
+++ models/products.js
@@ -5,25 +5,18 @@
 //Initialize mongoose
 const mongoose = require('mongoose');
 
-//Initialize mongoose-fuzzy-search
-const mongooseFuzzySearching = require('mongoose-fuzzy-searching');
-
 //Create Product Schema
 const productSchema = mongoose.Schema({
   name: {type: String, required: true},
-  price: {type: Number, required: false},
+  price: {type: Number, min: 0, required: true},
   description: {type: String},
   img: {type: String, required: false},
   material: {type: String, required: false},
   color: {type: String, required: false},
   strap: {type: String, required: false},
-  interchangeable: {type: Boolean, default: false}
+  interchangeable: {type: Boolean, default: false},
+  qty: {type: Number, min: 0, required: true}
 }, {timestamps: true});
-
-//Attach fuzzy searching to schema and specify which fields to fuzzy search for
-productSchema.plugin(mongooseFuzzySearching, {
-  fields: ['name', 'material', 'color', 'strap']
-});
 
 //Export Products model
 module.exports = mongoose.model('Product', productSchema);
