2f03f1f2176e741aa7bb4e3b7be82b6fc748e3d2
Henry Baum <henryandrewbaum@gmail.com>
2020-11-23 20:50:30 -0500

    Added write concern for all models

--- models/products.js
+++ models/products.js
@@ -6,16 +6,26 @@
 const mongoose = require('mongoose');
 
 //Create Product Schema
-const productSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  price: {type: Number, min: 0, required: true},
-  description: {type: String},
-  img: {type: String, required: false},
-  material: {type: String, required: false},
-  color: {type: String, required: false},
-  strap: {type: String, required: false},
-  qty: {type: Number, min: 0, required: true}
-}, {timestamps: true});
+const productSchema = mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    price: {type: Number, min: 0, required: true},
+    description: {type: String},
+    img: {type: String, required: false},
+    material: {type: String, required: false},
+    color: {type: String, required: false},
+    strap: {type: String, required: false},
+    qty: {type: Number, min: 0, required: true}
+  },  
+  {     
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 1000
+    },
+    timestamps: true 
+  }
+);
 
 //Export Products model
 module.exports = mongoose.model('Product', productSchema);
